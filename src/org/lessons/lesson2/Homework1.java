2fc0a040f155863e32c4698dd9b9e8f64f60feae
riddle671@yandex.ru <12345Xxx2>
2022-08-22 00:03:44 +0300

    изменение №2

--- src/org/lessons/lesson2/Homework1.java
+++ src/org/lessons/lesson2/Homework1.java
@@ -11,10 +11,10 @@ public class Homework1 {
 String result = hi.trim() + world.toLowerCase() + newLine;
 System.out.print(result.repeat(3));
 
-        int g1= 100;
-        int h1= 2;
-        float G1H1 = g1/ (h1* h1);
-        System.out.print(G1H1);
+        float g1 = 100;
+        float h1 = 2;
+        float g1h1 = g1 / (h1 * h1);
+        System.out.print(g1h1);
 
 
 
