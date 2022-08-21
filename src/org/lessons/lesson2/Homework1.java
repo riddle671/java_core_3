75acf7a3767e9e0b877c1820cbc484f430eaac59
riddle671@yandex.ru <12345Xxx2>
2022-08-21 23:35:44 +0300

    изменение №2

--- src/org/lessons/lesson2/Homework1.java
+++ src/org/lessons/lesson2/Homework1.java
@@ -1,14 +1,15 @@
 package org.lessons.lesson2;
 
 import java.awt.*;
+import java.util.Locale;
 
 public class Homework1 {
     public static void main(String[] args) {
         String hi = "                Hello ";
         String world = " WoRld!";
         char newLine = '\n';
-
-
+String result = hi.trim() + world.toLowerCase() + newLine;
+System.out.print(result.repeat(3));
 
 
     }
