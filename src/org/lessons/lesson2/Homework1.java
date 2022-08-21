07e9f15e45c361e4ca577296d81d92a3a9baaedd
riddle671@yandex.ru <12345Xxx2>
2022-08-21 22:08:58 +0300

    изменение №2

--- src/org/lessons/lesson2/Homework1.java
+++ src/org/lessons/lesson2/Homework1.java
@@ -1,4 +1,16 @@
 package org.lessons.lesson2;
 
+import java.awt.*;
+
 public class Homework1 {
+    public static void main(String[] args) {
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+
+
+
+
+    }
+
 }
