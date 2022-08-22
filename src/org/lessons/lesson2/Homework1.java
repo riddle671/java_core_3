3c5fd7d5d52204001aac7c47bea0d2e83eab3d60
riddle671 <riddle671@yandex.ru>
2022-08-22 19:16:24 +0300

    изменение 22.08 работа

--- src/org/lessons/lesson2/Homework1.java
+++ src/org/lessons/lesson2/Homework1.java
@@ -2,14 +2,15 @@ package org.lessons.lesson2;
 
 import java.awt.*;
 import java.util.Locale;
-// Задача 1
+
 public class Homework1 {
     public static void main(String[] args) {
+// Задача 1
         String hi = "                Hello ";
         String world = " WoRld!";
         char newLine = '\n';
-String result = hi.trim() + world.toLowerCase() + newLine;
-System.out.print(result.repeat(3));
+        String result = hi.trim() + world.toLowerCase() + newLine;
+        System.out.print(result.repeat(3));
 
 // Задача 2
         float g1 = 100;
@@ -17,7 +18,14 @@ System.out.print(result.repeat(3));
         float g1h1 = g1 / (h1 * h1);
         System.out.println(g1h1);
 
+// Задача 3
+        String str1 = new String ("a,b,c,d,e");
+        System.out.println(str1);
+        String str2 = new String("a,b,c,d,e".replace('d' , 'h'));
+        System.out.println(str2);
 
+        String str3 = new String(new char[] {'a', 'b', 'c', 'd', 'e'});
+        System.out.println(str3);
     }
-
 }
+
