35747251f8c958421738e4a67a208d868a37da06
riddle671 <riddle671@yandex.ru>
2022-08-22 08:36:10 +0300

    изменение №3

--- src/org/lessons/lesson2/Homework1.java
+++ src/org/lessons/lesson2/Homework1.java
@@ -2,7 +2,7 @@ package org.lessons.lesson2;
 
 import java.awt.*;
 import java.util.Locale;
-
+// Задача 1
 public class Homework1 {
     public static void main(String[] args) {
         String hi = "                Hello ";
@@ -10,6 +10,12 @@ public class Homework1 {
         char newLine = '\n';
 String result = hi.trim() + world.toLowerCase() + newLine;
 System.out.print(result.repeat(3));
+
+// Задача 2
+        float g1 = 100;
+        float h1 = 2;
+        float g1h1 = g1 / (h1 * h1);
+        System.out.println(g1h1);
 
 
     }
